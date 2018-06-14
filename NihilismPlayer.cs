481e69711eff5c4e0afc81d9f8948e47551259be
hamstar0 <webutek@gmail.com>
2018-06-14 04:10:14 -0700

    v2.0.0 * Corrected player load refactorings

--- NihilismPlayer.cs
+++ NihilismPlayer.cs
@@ -6,6 +6,12 @@ using Terraria.ModLoader;
 
 namespace Nihilism {
 	partial class NihilismPlayer : ModPlayer {
+		private bool IsModSettingsSynced = false;
+		private bool IsFiltersSynced = false;
+
+
+		////////////////
+
 		public override bool CloneNewInstances { get { return false; } }
 		
 		public override void Initialize() {
@@ -15,6 +21,27 @@ namespace Nihilism {
 		public override void clientClone( ModPlayer client_clone ) {
 			var clone = (NihilismPlayer)client_clone;
 			//clone.HasEnteredWorld = this.HasEnteredWorld;
+		}
+
+
+		////////////////
+
+		public override void SyncPlayer( int to_who, int from_who, bool new_player ) {
+			if( Main.netMode == 2 ) {
+				if( to_who == -1 && from_who == this.player.whoAmI ) {
+					this.OnEnterWorldForServer();
+				}
+			}
+		}
+
+		public override void OnEnterWorld( Player player ) {
+			if( player.whoAmI != this.player.whoAmI ) { return; }
+			
+			if( Main.netMode == 0 ) {
+				this.OnEnterWorldForSingle();
+			} else if( Main.netMode == 1 ) {
+				this.OnEnterWorldForClient();
+			}
 		}
 
 
--- NihilismPlayer_Load.cs
+++ NihilismPlayer_Load.cs
@@ -1,58 +1,22 @@
 ﻿using HamstarHelpers.DebugHelpers;
-using HamstarHelpers.PlayerHelpers;
-using HamstarHelpers.TmlHelpers;
 using HamstarHelpers.Utilities.Network;
 using Nihilism.Data;
 using Nihilism.NetProtocol;
-using Terraria;
 using Terraria.ModLoader;
 
 
 namespace Nihilism {
 	partial class NihilismPlayer : ModPlayer {
-		private bool IsModSettingsSynced = false;
-		private bool IsFiltersSynced = false;
-
-
-		////////////////
-
-		public override void SyncPlayer( int to_who, int from_who, bool new_player ) {
-			if( Main.netMode == 2 ) {
-				if( to_who == -1 && from_who == this.player.whoAmI ) {
-					var mymod = (NihilismMod)this.mod;
-					var myworld = mymod.GetModWorld<NihilismWorld>();
-
-					this.OnEnterWorldForServer( mymod, this.player );
-				}
-			}
-		}
-		
-		public override void OnEnterWorld( Player player ) {
-			if( player.whoAmI != this.player.whoAmI ) { return; }
-
+		internal void OnEnterWorldForSingle() {
 			var mymod = (NihilismMod)this.mod;
+			var myworld = this.mod.GetModWorld<NihilismWorld>();
 
-			if( Main.netMode == 0 ) {
-				if( !mymod.SuppressAutoSaving ) {
-					if( !mymod.ConfigJson.LoadFile() ) {
-						mymod.ConfigJson.SaveFile();
-						LogHelpers.Log( "Nihilism config " + NihilismConfigData.ConfigVersion.ToString() + " created (ModPlayer.OnEnterWorld())." );
-					}
+			if( !mymod.SuppressAutoSaving ) {
+				if( !mymod.ConfigJson.LoadFile() ) {
+					mymod.ConfigJson.SaveFile();
+					LogHelpers.Log( "Nihilism config " + NihilismConfigData.ConfigVersion.ToString() + " created (ModPlayer.OnEnterWorld())." );
 				}
 			}
-
-			if( Main.netMode == 0 ) {
-				this.OnEnterWorldForSingle( mymod, player );
-			} else if( Main.netMode == 1 ) {
-				this.OnEnterWorldForClient( mymod, player );
-			}
-		}
-
-
-		////////////////
-
-		internal void OnEnterWorldForSingle( NihilismMod mymod, Player player ) {
-			var myworld = this.mod.GetModWorld<NihilismWorld>();
 
 			myworld.Logic.PostFiltersLoad( mymod );
 
@@ -60,14 +24,14 @@ namespace Nihilism {
 			this.FinishFiltersSync();
 		}
 
-		internal void OnEnterWorldForClient( NihilismMod mymod, Player player ) {
+		internal void OnEnterWorldForClient() {
 			PacketProtocol.QuickRequestToServer<ModSettingsProtocol>();
 			PacketProtocol.QuickRequestToServer<FiltersProtocol>();
 		}
 
-		internal void OnEnterWorldForServer( NihilismMod mymod, Player player ) {
-			this.FinishModSettingsSync();
-			this.FinishFiltersSync();
+		internal void OnEnterWorldForServer() {
+			this.IsModSettingsSynced = true;
+			this.IsFiltersSynced = true;
 		}
 
 
